f92e37d464a8b72b33e8191a2bd59ce6946386f8
Mohit Sharma <140743084+MohiTSh4rmA@users.noreply.github.com>
2025-08-06 21:31:18 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CreatePost from './pages/CreatePost';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/create' element={<CreatePost />} />
+      </Routes>
+    </Router>
+  );
+}
